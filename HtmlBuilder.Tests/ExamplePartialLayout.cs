7737b8636f5fba6035af2c01ebdef797d97863e2
James Kelly <james.kelly.99@gmail.com>
2024-05-12 10:49:03 +1000

    add a partial layout for testing

--- HtmlBuilder.Tests/ExamplePartialLayout.cs
+++ HtmlBuilder.Tests/ExamplePartialLayout.cs
@@ -1,3 +1,5 @@
+using Shouldly;
+
 namespace HtmlBuilder.Tests;
 
 public class ExamplePartialLayout : ExampleLayout
@@ -8,5 +10,56 @@ public class ExamplePartialLayout : ExampleLayout
 		body.Add(new Footer(new Class("footer")));
 		return body;
 	}
+}
+
+public class ViewModelLayout : ExampleLayout
+{
+	public ViewModelLayout(IViewModel vm) : base()
+	{
+		Body().Add(vm.AsElement());
+	}
+}
+
+public class ViewModelTests
+{
+	[Fact]
+	public void ViewModel_ShouldBeAddedIntoLayout()
+	{
+		var vm = new TestViewModel();
+		vm.Name = "test name";
+		vm.Address = "1 test way, test city, test postcode";
+		vm.Phone = "01234 567890";
+
+		var layout = new ViewModelLayout(vm);
+
+		layout.ToString().ShouldBe("something");
+
+	}
+}
+
+public class TestViewModel : IViewModel
+{
+	public string Name {get;set;}  
+	public string Address {get;set;}
+	public string Phone {get;set;}
+
+    public Element AsElement()
+    {
+		var div = new Div();
+
+		var sp1 = new Span(new Text(Name));
+		var sp2 = new Span(new Text(Address));
+		var sp3 = new Span(new Text(Phone));
+		div.Add(sp1);
+		div.Add(sp2);
+		div.Add(sp3);
+		return div;
+    }
+}
+
+
+public interface IViewModel
+{
+	Element AsElement();
 }
 
