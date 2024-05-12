edabf59d87459b086ea11d951fa70328a1186cdc
James Kelly <james.kelly.99@gmail.com>
2024-05-12 10:48:38 +1000

    add ctor overloads and new span element

--- HtmlBuilder.Tests/Elements/Div.cs
+++ HtmlBuilder.Tests/Elements/Div.cs
@@ -5,4 +5,14 @@ public class Div : Element
 	public Div(Class @class) : base(@class)
 	{
 	}
+
+	public Div(): base()
+	{
+
+	}
+
+	public void AddRange(List<Element> elements)
+	{
+		AddRange(elements);
+	}
 }	
---
+++ HtmlBuilder.Tests/Elements/Span.cs
@@ -0,0 +1,19 @@
+namespace HtmlBuilder.Tests;
+
+public class Span : Element
+{
+	public Span(Class @class): base(@class)
+	{
+
+	}
+
+	public Span(): base()
+	{
+
+	}
+
+	public Span(Text value): base(value)
+	{
+	}
+}
+
