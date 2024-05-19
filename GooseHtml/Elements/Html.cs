99bb14cd220b8dd12498ab42a5567e10cfd36cf2
James Kelly <james.kelly.99@gmail.com>
2024-05-19 15:17:24 +1000

    prepend the doctype to the output

--- GooseHtml/Elements/Html.cs
+++ GooseHtml/Elements/Html.cs
@@ -1,3 +1,4 @@
+
 namespace GooseHtml;
 
 public class Html : Element
@@ -8,8 +9,12 @@ public class Html : Element
 
 	public Html(string name): base(name)
 	{
-
 	}
 
+    public override string ToString()
+    {
+		//add the html doctype
+		return  base.ToString().Insert(0, "<!DOCTYPE html>");
+    }
 }
 
