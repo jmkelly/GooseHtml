e0071d49ae8fe43eeff29b5766e586535ce1dafb
James Kelly <james.kelly.99@gmail.com>
2024-10-28 21:20:58 +1100

    refactor to tidy

--- GooseHtml.Samples/Htmx/StockCode.cs
+++ GooseHtml.Samples/Htmx/StockCode.cs
@@ -1,20 +1,19 @@
-class StockCode
+class StockCode(string Code, decimal Price, Trend Trend, DateTime CurrentAt)
 {
-	internal string Code {get;private set;}
-	internal decimal Price {get;private set;}
-	internal Trend Trend {get;private set;}
-	internal DateTime CurrentAt {get;private set;}
+    public string Code { get; } = Code;
+    public decimal Price { get; } = Price;
+    public Trend Trend { get; } = Trend;
+    public DateTime CurrentAt { get; } = CurrentAt;
 
-	internal static StockCode Create(string code, decimal price, Trend trend, DateTime currentAt)
-	{
-		return new StockCode {Code = code, Price = price, Trend = trend, CurrentAt = currentAt};
-	}
-
-	internal static StockCode CreateRandomCode()
+    internal static StockCode CreateRandomCode()
 	{
 		var codeStr = new string(Enumerable.Repeat("ABCDEFGHIJKLMNOPQRSTUVWXYZ", 3).Select(s => s[new Random().Next(s.Length)]).ToArray());
 
-		var code = new StockCode {Code = codeStr, Price = new Random().Next(100), Trend = new Random().Next(3) == 0 ? Trend.Up : new Random().Next(3) == 1 ? Trend.Down : Trend.Flat, CurrentAt = DateTime.UtcNow};
+		var code = new StockCode (Code : codeStr, 
+				Price : new Random().Next(100), 
+				Trend : new Random().Next(3) == 0 ? Trend.Up : new Random().Next(3) == 1 ? Trend.Down : Trend.Flat, 
+				CurrentAt : DateTime.UtcNow
+			);
 
 		ArgumentNullException.ThrowIfNull(code);
 
