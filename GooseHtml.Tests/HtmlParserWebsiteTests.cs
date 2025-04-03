3869e38633ad242247ce45e7eb05bcd3cd9a785d
James Kelly <james@locationsolve.com>
2025-04-03 20:47:38 +1100

    get all the websites

--- GooseHtml.Tests/HtmlParserWebsiteTests.cs
+++ GooseHtml.Tests/HtmlParserWebsiteTests.cs
@@ -108,8 +108,8 @@ public class HtmlParserWebTest
 			// Parse
 			Console.WriteLine($"requested {url} ok");
 
-			//var parser = new HtmlParser(html); // Ensure you have an HtmlParser class
-			//parser.Parse();
+			var parser = new HtmlParser(html); // Ensure you have an HtmlParser class
+			parser.Parse();
 		}
 			//var shortcurcuit = File.ReadAllText("test_pages/html.spec.whatwg.org__2025-03.html");
 			//var parser = new HtmlParser(shortcurcuit); // Ensure you have an HtmlParser class
